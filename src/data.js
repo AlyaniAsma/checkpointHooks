ec18758d73fd9742ecf271e3311f68cde741871c
AlyaniAsma <asmaalyani8@gmail.com>
2022-03-24 13:15:29 +0100

    hooks project

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const movies = [
+  {
+    title: "Mulan ",
+    description: "1998",
+    rating: "20",
+    posterURL: "http://www.movienewsletters.net/photos/015352R1.jpg",
+  },
+  {
+    title: "Cinderella",
+    description: "1950",
+    rating: "58",
+    posterURL:
+      "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSQbo47YugWSvYcEztHm7KdCCiRMcVWpH28eKfup7zJ2M7SwVV_",
+  },
+  {
+    title: "Aladdin",
+    description: "1992",
+    rating: "70",
+    posterURL:
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQPxPiI8SWZ42gpv6oqxG9vcO4hANiMBCGA8hso25I_edHRwh_t",
+  },
+];
